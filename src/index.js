edbf41c912e1a8239c603ae0aa2e4d9eb9fb144e
Erhan Ertem <eertem@gmail.com>
2023-06-17 22:25:41 +0300

    Create a customizable star rating component

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		{/* <App /> */}
-		<StarRating />
+		<StarRating maxRating={5} />
+		{/* <StarRating maxRating={10} /> */}
+		{/* <StarRating /> */}
 	</React.StrictMode>,
 )
