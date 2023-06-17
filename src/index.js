cee9ff821bce2e2d47e4eb75064da8e90167aa3a
Erhan Ertem <eertem@gmail.com>
2023-06-17 23:05:49 +0300

    Elaborate customizable star rating component and establish type checking

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 // import './index.css'
 // import App from './App'
 
 import StarRating from './StarRating'
 
+function Test() {
+	const [movieRating, setMovieRating] = useState(0)
+
+	return (
+		<div>
+			<StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+			<p>This movie is rated {movieRating} stars</p>
+		</div>
+	)
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		{/* <App /> */}
-		<StarRating maxRating={5} />
+		<StarRating
+			maxRating={5}
+			messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+			defaultRating={3}
+		/>
+		<StarRating size={24} color="red" maxRating={10} className="test" />
 		{/* <StarRating maxRating={10} /> */}
 		{/* <StarRating /> */}
+
+		<Test />
 	</React.StrictMode>,
 )
