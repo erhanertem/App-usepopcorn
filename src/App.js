a5769076eaa1d6c5e941fcac02a1656544ab9224
Erhan Ertem <eertem@gmail.com>
2023-06-19 17:46:26 +0300

    Error handling with fetching data

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
 import { useEffect, useState } from 'react'
 
-// const tempMovieData = [
-// 	{
-// 		imdbID: 'tt1375666',
-// 		Title: 'Inception',
-// 		Year: '2010',
-// 		Poster:
-// 			'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-// 	},
-// 	{
-// 		imdbID: 'tt0133093',
-// 		Title: 'The Matrix',
-// 		Year: '1999',
-// 		Poster:
-// 			'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-// 	},
-// 	{
-// 		imdbID: 'tt6751668',
-// 		Title: 'Parasite',
-// 		Year: '2019',
-// 		Poster:
-// 			'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
-// 	},
-// ]
-
-// const tempWatchedData = [
-// 	{
-// 		imdbID: 'tt1375666',
-// 		Title: 'Inception',
-// 		Year: '2010',
-// 		Poster:
-// 			'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-// 		runtime: 148,
-// 		imdbRating: 8.8,
-// 		userRating: 10,
-// 	},
-// 	{
-// 		imdbID: 'tt0088763',
-// 		Title: 'Back to the Future',
-// 		Year: '1985',
-// 		Poster:
-// 			'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-// 		runtime: 116,
-// 		imdbRating: 8.5,
-// 		userRating: 9,
-// 	},
-// ]
-
 const average = arr =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)
 
-const APIKEY = '327c17b6'
+const APIKEY = '327c17b6' //@erhan1
 
 export default function App() {
 	const [movies, setMovies] = useState([])
 	const [watched, setWatched] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState('')
+	// const query = 'interstellar'
+	const query = 'xcxxcx'
 
 	// // Version #1 fetch.then version
 	// useEffect(function () {
@@ -75,11 +32,26 @@ export default function App() {
 	// }, [])
 	useEffect(function () {
 		return async function fetchMovies() {
-			const res = await fetch(
-				`http://www.omdbapi.com/?apikey=${APIKEY}&s=interstellar`,
-			)
-			const data = await res.json()
-			setMovies(data.Search)
+			try {
+				setIsLoading(true)
+				const res = await fetch(
+					`http://www.omdbapi.com/?apikey=${APIKEY}&s=${query}}`,
+				)
+				console.log(res)
+				if (!res.ok)
+					throw new Error('Something went wrong with fetching movies')
+
+				const data = await res.json()
+				if (data.Response === 'False') throw new Error('Movie not found')
+
+				setMovies(data.Search)
+				console.log(data)
+			} catch (err) {
+				console.log(err.message)
+				setError(err.message)
+			} finally {
+				setIsLoading(false)
+			}
 		}
 	}, [])
 
@@ -93,7 +65,10 @@ export default function App() {
 			<Main>
 				{/* Alternative #1 Proppping as children to cut down prop drilling */}
 				<Box>
-					<MovieList movies={movies} />
+					{/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+					{isLoading && <Loader />}
+					{isLoading && !error && <MovieList movies={movies} />}
+					{isLoading && error && <ErrorMessage message={error} />}
 				</Box>
 				<Box>
 					<>
@@ -113,6 +88,19 @@ export default function App() {
 				/> */}
 			</Main>
 		</>
+	)
+}
+
+function Loader() {
+	return <p className="loader">Loading...</p>
+}
+
+function ErrorMessage({ message }) {
+	return (
+		<p className="error">
+			<span>⛔</span>
+			{message}
+		</p>
 	)
 }
 
