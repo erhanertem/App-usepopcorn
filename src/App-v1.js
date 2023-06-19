4b7f2c6d0e3201a87db89c1dbef3032831e855bf
Erhan Ertem <eertem@gmail.com>
2023-06-19 15:28:27 +0300

    Fetching data from omdbapi w/ useEffect

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,87 +1,58 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
-// const tempMovieData = [
-// 	{
-// 		imdbID: 'tt1375666',
-// 		Title: 'Inception',
-// 		Year: '2010',
-// 		Poster:
-// 			'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-// 	},
-// 	{
-// 		imdbID: 'tt0133093',
-// 		Title: 'The Matrix',
-// 		Year: '1999',
-// 		Poster:
-// 			'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-// 	},
-// 	{
-// 		imdbID: 'tt6751668',
-// 		Title: 'Parasite',
-// 		Year: '2019',
-// 		Poster:
-// 			'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
-// 	},
-// ]
+const tempMovieData = [
+	{
+		imdbID: 'tt1375666',
+		Title: 'Inception',
+		Year: '2010',
+		Poster:
+			'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+	},
+	{
+		imdbID: 'tt0133093',
+		Title: 'The Matrix',
+		Year: '1999',
+		Poster:
+			'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+	},
+	{
+		imdbID: 'tt6751668',
+		Title: 'Parasite',
+		Year: '2019',
+		Poster:
+			'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
+	},
+]
 
-// const tempWatchedData = [
-// 	{
-// 		imdbID: 'tt1375666',
-// 		Title: 'Inception',
-// 		Year: '2010',
-// 		Poster:
-// 			'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-// 		runtime: 148,
-// 		imdbRating: 8.8,
-// 		userRating: 10,
-// 	},
-// 	{
-// 		imdbID: 'tt0088763',
-// 		Title: 'Back to the Future',
-// 		Year: '1985',
-// 		Poster:
-// 			'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-// 		runtime: 116,
-// 		imdbRating: 8.5,
-// 		userRating: 9,
-// 	},
-// ]
+const tempWatchedData = [
+	{
+		imdbID: 'tt1375666',
+		Title: 'Inception',
+		Year: '2010',
+		Poster:
+			'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+		runtime: 148,
+		imdbRating: 8.8,
+		userRating: 10,
+	},
+	{
+		imdbID: 'tt0088763',
+		Title: 'Back to the Future',
+		Year: '1985',
+		Poster:
+			'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+		runtime: 116,
+		imdbRating: 8.5,
+		userRating: 9,
+	},
+]
 
 const average = arr =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)
 
-const APIKEY = '327c17b6'
-
 export default function App() {
-	const [movies, setMovies] = useState([])
-	const [watched, setWatched] = useState([])
-
-	// // Version #1 fetch.then version
-	// useEffect(function () {
-	// 	fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=interstellar`)
-	// 		.then(res => res.json())
-	// 		.then(data => setMovies(data.Search))
-	// }, [])
-	// Version #2 Async-await version
-	// useEffect(function () {
-	// 	async function fetchMovies() {
-	// 		const res = await fetch(
-	// 			`http://www.omdbapi.com/?apikey=${APIKEY}&s=interstellar`,
-	// 		)
-	// 		const data = await res.json()
-	// 		setMovies(data.Search)
-	// 	}
-	// 	fetchMovies()
-	// }, [])
-	useEffect(function () {
-		return async function fetchMovies() {
-			const res = await fetch(
-				`http://www.omdbapi.com/?apikey=${APIKEY}&s=interstellar`,
-			)
-			const data = await res.json()
-			setMovies(data.Search)
-		}
-	}, [])
+	const [movies, setMovies] = useState(tempMovieData)
+	const [watched, setWatched] = useState(tempWatchedData)
 
 	return (
 		<>
--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
-// import './index.css'
-// import App from './App'
-
-import StarRating from './StarRating'
-
-function Test() {
-	const [movieRating, setMovieRating] = useState(0)
-
-	return (
-		<div>
-			<StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-			<p>This movie is rated {movieRating} stars</p>
-		</div>
-	)
-}
+import './index.css'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		{/* <App /> */}
-		<StarRating
-			maxRating={5}
-			messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
-			defaultRating={3}
-		/>
-		<StarRating size={24} color="red" maxRating={10} className="test" />
-		{/* <StarRating maxRating={10} /> */}
-		{/* <StarRating /> */}
-
-		<Test />
+		<App />
 	</React.StrictMode>,
 )
