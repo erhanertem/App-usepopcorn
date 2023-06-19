93883ae738b76c9914af456c6427025eac6dd37b
Erhan Ertem <eertem@gmail.com>
2023-06-19 23:38:08 +0300

    Implement movie selec/deselect

--- src/App.js
+++ src/App.js
@@ -12,9 +12,18 @@ export default function App() {
 	const [isLoading, setIsLoading] = useState(false)
 	const [error, setError] = useState('')
 	const [query, setQuery] = useState('')
+	const [selectedId, setSelectedId] = useState(null)
 
 	// const tempQuery = 'interstellar'
 	// const tempQuery = 'xcxxcx'
+
+	function handleSelectMovie(id) {
+		setSelectedId(selectedId => (id === selectedId ? null : id))
+	}
+
+	function handleCloseMovie() {
+		setSelectedId(null)
+	}
 
 	// useEffect(function () {
 	// 	console.log('After Initial Render')
@@ -87,14 +96,23 @@ export default function App() {
 				<Box>
 					{/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
 					{isLoading && <Loader />}
-					{!isLoading && !error && <MovieList movies={movies} />}
+					{!isLoading && !error && (
+						<MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+					)}
 					{error && <ErrorMessage message={error} />}
 				</Box>
 				<Box>
-					<>
-						<WatchedSummary watched={watched} />
-						<WatchedMoviesList watched={watched} />
-					</>
+					{selectedId ? (
+						<MovieDetails
+							selectedId={selectedId}
+							onCloseMovie={handleCloseMovie}
+						/>
+					) : (
+						<>
+							<WatchedSummary watched={watched} />
+							<WatchedMoviesList watched={watched} />
+						</>
+					)}
 				</Box>
 				{/* Alternative #2 Passing Elements as Props */}
 				{/* <Box element={<MovieList movies={movies} />} />
@@ -178,19 +196,19 @@ function Box({ children }) {
 	)
 }
 
-function MovieList({ movies }) {
+function MovieList({ movies, onSelectMovie }) {
 	return (
-		<ul className="list">
+		<ul className="list list-movies">
 			{movies?.map(movie => (
-				<Movie movie={movie} key={movie.imdbID} />
+				<Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
 			))}
 		</ul>
 	)
 }
 
-function Movie({ movie }) {
+function Movie({ movie, onSelectMovie }) {
 	return (
-		<li>
+		<li onClick={() => onSelectMovie(movie.imdbID)}>
 			<img src={movie.Poster} alt={`${movie.Title} poster`} />
 			<h3>{movie.Title}</h3>
 			<div>
@@ -200,6 +218,17 @@ function Movie({ movie }) {
 				</p>
 			</div>
 		</li>
+	)
+}
+
+function MovieDetails({ selectedId, onCloseMovie }) {
+	return (
+		<div className="details">
+			<button className="btn-back" onClick={onCloseMovie}>
+				&larr;
+			</button>
+			{selectedId}
+		</div>
 	)
 }
 
