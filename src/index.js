10d5131597b9855b6a09bd513944a727638c2679
Erhan Ertem <eertem@gmail.com>
2023-06-22 11:59:46 +0300

    Roll back to main project and related amendments on files

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-import { useState } from 'react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import './index.css'
-// import App from './App'
-
-import StarRating from './StarRating'
-
-function Test() {
-	const [movieRating, setMovieRating] = useState(0)
-	return (
-		<div>
-			<StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-			<p>This movie is rated {movieRating} stars.</p>
-		</div>
-	)
-}
+import './index.css'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		{/* <App /> */}
-		<StarRating
-			maxRating={5}
-			messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
-		/>
-		<StarRating size={24} color="red" className="test" />
-		<Test />
+		<App />
 	</React.StrictMode>,
 )
