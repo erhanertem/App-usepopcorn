017cdd71632a04d0a93ca0778db78403c6eb7303
Erhan Ertem <eertem@gmail.com>
2023-06-22 11:42:31 +0300

    Create a customizable star rating component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import './index.css'
@@ -5,10 +6,25 @@ import ReactDOM from 'react-dom/client'
 
 import StarRating from './StarRating'
 
+function Test() {
+	const [movieRating, setMovieRating] = useState(0)
+	return (
+		<div>
+			<StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+			<p>This movie is rated {movieRating} stars.</p>
+		</div>
+	)
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		{/* <App /> */}
-		<StarRating />
+		<StarRating
+			maxRating={5}
+			messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+		/>
+		<StarRating size={24} color="red" className="test" />
+		<Test />
 	</React.StrictMode>,
 )
